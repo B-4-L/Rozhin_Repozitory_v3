b214f47275430969e46d1af970df87718dcda45f
B-4-L <147860744+B-4-L@users.noreply.github.com>
2023-12-18 21:27:34 +0300

    Add files via upload

---
+++ automatitation.bat
@@ -0,0 +1,4 @@
+"C:\Program Files (x86)\Microsoft Visual Studio 9.0\Common7\IDE\
+VCExpress.exe" project_test.sln /build Debug
+"C:\Program Files\doxygen\bin\doxygen.exe" myConfig
+"C:\Program Files\Inno Setup 6\ISCC.exe" build.iss
